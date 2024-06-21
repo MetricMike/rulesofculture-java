5630426df6cf4a2e47de003225c1601f8e6caee8
Michael Weigle <michael.weigle@gmail.com>
2024-06-20 20:26:11 -0400

    init commit

---
+++ src/main/java/com/metricmike/rulesofculture/RulesofcultureApplication.java
@@ -0,0 +1,13 @@
+package com.metricmike.rulesofculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesofcultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesofcultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metricmike/rulesofculture/RulesofcultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metricmike.rulesofculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesofcultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
